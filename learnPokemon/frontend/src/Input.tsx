229b823a31168c714d6e22475fb391632dffbd4f
M-Feale <marjolainefeale@gmail.com>
2024-11-12 14:08:36 -0500

    [WIP](Input) start implementation of Input component

---
+++ learnPokemon/frontend/src/Input.tsx
@@ -0,0 +1,17 @@
+const Input = () => {
+	return (
+		<input
+			type="text"
+			placeholder="Write your guess here !"
+			style={{
+				width: "50vw",
+				fontSize: "60px",
+				fontFamily: "Lalezar",
+				fontWeight: 400,
+				fontStyle: "normal",
+			}}
+		/>
+	);
+};
+
+export default Input;
