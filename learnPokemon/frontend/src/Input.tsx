90775806b26223f3a1ee787b10107795c9d609dd
M-Feale <marjolainefeale@gmail.com>
2024-11-12 15:57:08 -0500

    [feat](Input) complete input styling

--- learnPokemon/frontend/src/Input.tsx
+++ learnPokemon/frontend/src/Input.tsx
@@ -1,17 +1,42 @@
+import { styled } from "styled-components";
+
 const Input = () => {
 	return (
-		<input
-			type="text"
-			placeholder="Write your guess here !"
-			style={{
-				width: "50vw",
-				fontSize: "60px",
-				fontFamily: "Lalezar",
-				fontWeight: 400,
-				fontStyle: "normal",
-			}}
-		/>
+		<LabelAndInput>
+			<input type="text" id="guessInput" placeholder="Write your guess here !" />
+			<label htmlFor="guessInput">Write your guess here !</label>
+		</LabelAndInput>
 	);
 };
+
+const LabelAndInput = styled.div`
+	position: relative;
+
+	& input {
+		width: 50vw;
+		font-size: 60px;
+		font-family: "Lalezar";
+		font-weight: 400;
+		font-style: "normal";
+		outline: 50px solid #363b81;
+	}
+
+	& label {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		font-size: 60px;
+		font-family: "Lalezar";
+		font-weight: 400;
+		font-style: normal;
+		left: 4px;
+		color: rgb(118, 118, 118); // To be a grey like the palceholder
+	}
+
+	& input:focus ~ label,
+	input:not(:placeholder-shown) ~ label {
+		z-index: -1;
+	}
+`;
 
 export default Input;
