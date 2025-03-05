823d66f91d8bc0f60b66949acd07805a17f46b4e
M-Feale <marjolainefeale@gmail.com>
2025-03-05 15:29:17 -0500

    [test](createUniqueRandomInts) implement unit test

--- learnPokemon/backend/tests/createUniqueRandomInts.test.ts
+++ learnPokemon/backend/tests/createUniqueRandomInts.test.ts
@@ -1,36 +1,43 @@
 import { createUniqueRandomInts } from "../src/utils";
 
-it("should throw an error when the 'howMany' is smaller than the range", () => {
-	const minRange = 1;
-	const maxRange = 2;
-	const howMany = 10;
-	expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
+describe("argument validity", () => {
+	test("throws an error when the 'howMany' is smaller than the range", () => {
+		const minRange = 1;
+		const maxRange = 2;
+		const howMany = 10;
+		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
+	});
+
+	test("throws an error when the 'maxRange' is smaller than the 'minRange'", () => {
+		const minRange = 20;
+		const maxRange = 2;
+		const howMany = 10;
+		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
+	});
+
+	test("throws an error when the 'howMany' is smaller than 1", () => {
+		const minRange = 1;
+		const maxRange = 10;
+		const howMany = -5;
+		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
+	});
+
+	test("throws an error when the arguments aren't integers", () => {
+		const minRange = 1.2;
+		const maxRange = 10.5;
+		const howMany = 5.6;
+		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
+	});
 });
 
-it("should throw an error when the 'maxRange' is smaller than the 'minRange'", () => {
-	const minRange = 20;
-	const maxRange = 2;
-	const howMany = 10;
-	expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
-});
+describe("return validity", () => {
+	test("returns an array of the same length as specified in the arguments", () => {
+		const minRange = 1;
+		const maxRange = 10;
+		const howMany = 5;
 
-it("should throw an error when the 'howMany' is smaller than 1", () => {
-	const minRange = 1;
-	const maxRange = 10;
-	const howMany = -5;
-	expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
-});
+		const fnReturn = createUniqueRandomInts(howMany, minRange, maxRange);
 
-it("should throw an error when the arguments aren't integers", () => {
-	const minRange = 1.2;
-	const maxRange = 10.5;
-	const howMany = 5.6;
-	expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
+		expect(fnReturn).toHaveLength(howMany);
+	});
 });
-
-// it("should return an em", () => {
-// 	const minRange = 1;
-// 	const maxRange = 10;
-// 	const howMany = -5;
-// 	expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
-// });
