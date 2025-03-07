b82eb9a4ba6daf126670703d781725a04bd49339
M-Feale <marjolainefeale@gmail.com>
2025-03-07 13:32:05 -0500

    [refactor](unit tests) change naming convention to follow Mae's guidelines

--- learnPokemon/backend/tests/utils.test.ts
+++ learnPokemon/backend/tests/utils.test.ts
@@ -1,13 +1,29 @@
 import { expect, describe, test, jest } from "@jest/globals";
-import { createUniqueRandomInts } from "../src/utils";
+import { createUniqueRandomInts, getRandomIntInclusive } from "../src/utils";
 import * as allUtils from "../src/utils";
 
-describe("argument validity", () => {
+describe("#getRandomIntInclusive", () => {
+	test("generates a random number in a range, including the min and max", () => {
+		// Arrange or Scaffolding
+		const minNumber = 0;
+		const maxNumber = 5;
+
+		// Act or Calling
+		const randomNumber = getRandomIntInclusive(minNumber, maxNumber);
+
+		// Assert or Assertion
+		expect(randomNumber).toBeGreaterThanOrEqual(minNumber);
+		expect(randomNumber).toBeLessThanOrEqual(maxNumber);
+	});
+});
+
+describe("#createUniqueRandomInts", () => {
 	test("throws an error when the 'howMany' is bigger than the range", () => {
 		const minRange = 1;
 		const maxRange = 2;
 		const howMany = 10;
 
+		// Wrapped in an anonymous function because since it's about testing for an error, calling the function itself it going to crash the code.
 		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
 	});
 
@@ -23,6 +39,7 @@ describe("argument validity", () => {
 		const minRange = 1;
 		const maxRange = 10;
 		const howMany = -5;
+
 		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
 	});
 
@@ -30,22 +47,11 @@ describe("argument validity", () => {
 		const minRange = 1.2;
 		const maxRange = 10.5;
 		const howMany = 5.6;
+
 		expect(() => createUniqueRandomInts(howMany, minRange, maxRange)).toThrow(RangeError);
 	});
-});
 
-describe("return validity", () => {
-	test("returns an array of the same length as specified in the arguments", () => {
-		const minRange = 1;
-		const maxRange = 10;
-		const howMany = 5;
-
-		const fnReturn = createUniqueRandomInts(howMany, minRange, maxRange);
-
-		expect(fnReturn).toHaveLength(howMany);
-	});
-
-	test("should call getRandomIntInclusive", () => {
+	test("calls getRandomIntInclusive", () => {
 		jest.spyOn(allUtils, "getRandomIntInclusive");
 		const minRange = 1;
 		const maxRange = 10;
@@ -68,5 +74,15 @@ describe("return validity", () => {
 		const result = createUniqueRandomInts(howMany, minRange, maxRange);
 
 		expect(result).toEqual([3, 2, 1]);
+	});
+
+	test("returns an array of the same length as specified in the arguments", () => {
+		const minRange = 1;
+		const maxRange = 10;
+		const howMany = 5;
+
+		const fnReturn = createUniqueRandomInts(howMany, minRange, maxRange);
+
+		expect(fnReturn).toHaveLength(howMany);
 	});
 });
