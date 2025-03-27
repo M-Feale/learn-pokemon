a182fa46f85c20f44757d1fbfdb26f52ba3f4431
M-Feale <marjolainefeale@gmail.com>
2025-03-27 14:20:26 -0400

    [WIP](handlers.test) WIP createGameSession handler unit tests

--- learnPokemon/backend/tests/handlers.test.ts
+++ learnPokemon/backend/tests/handlers.test.ts
@@ -4,11 +4,18 @@ import { helloWorld } from "../src/handlers";
 import type { Request as ExpressRequest, Response as ExpressResponse } from "express";
 
 describe("#helloWorld", () => {
-	test('it should call res send method with the string "Hello World"', async () => {
+	test('calls res send method with the string "Hello World"', async () => {
 		const mockRes = { send: jest.fn() } as unknown as ExpressResponse;
 
 		await helloWorld(undefined as unknown as ExpressRequest, mockRes, () => {});
 
 		expect(mockRes.send).toHaveBeenCalledWith("Hello World");
+	});
+});
+
+describe("#createGameSession", () => {
+	test("", async () => {
+		const mockClient = { connect: jest.fn(), query: jest.fn(), end: jest.fn() };
+		const mockRes = { status: jest.fn() } as unknown as ExpressResponse;
 	});
 });
